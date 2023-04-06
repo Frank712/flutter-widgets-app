877de321c9efdedb72688dd2d390e24fea0e47b9
Francisco Lino <franksuriel_7@hotmail.com>
2023-04-06 15:41:57 -0600

    ADD screen and drum

---
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SnackbarScreen extends StatelessWidget {
+  final name = 'snackbar_screen';
+  const SnackbarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
