15e05b347c930008d1b705183508ba24cec7545d
Francisco Lino <franksuriel_7@hotmail.com>
2023-04-07 23:44:48 -0600

    ADD ui controls

--- lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -1,11 +1,86 @@
 import 'package:flutter/material.dart';
 
 class UiControlsScreen extends StatelessWidget {
-  final name = 'ui_controls_screen';
+  static const name = 'ui_controls_screen';
   const UiControlsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('UI Controls')),
+      body: const _UIControlsView(),
+    );
+  }
+}
+
+class _UIControlsView extends StatefulWidget {
+  const _UIControlsView();
+
+  @override
+  State<_UIControlsView> createState() => _UIControlsViewState();
+}
+
+enum Transportation { car, plane, boat, submarine }
+
+class _UIControlsViewState extends State<_UIControlsView> {
+  bool isDeveloper = true;
+  Transportation selectedTranportation = Transportation.car;
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const ClampingScrollPhysics(),
+      children: [
+        SwitchListTile(
+          value: isDeveloper,
+          title: const Text('Developer Mode'),
+          subtitle: const Text('Controles adicionales'),
+          onChanged: (value) => setState(() {
+            isDeveloper = !isDeveloper;
+          }),
+        ),
+        ExpansionTile(
+          title: const Text('Tranportation vehicle'),
+          subtitle: Text('$selectedTranportation'),
+          children: [
+            RadioListTile(
+              title: const Text('By car'),
+              subtitle: const Text('Viajar por carro'),
+              value: Transportation.car,
+              groupValue: selectedTranportation,
+              onChanged: (value) => setState(() {
+                selectedTranportation = Transportation.car;
+              }),
+            ),
+            RadioListTile(
+              title: const Text('By boat'),
+              subtitle: const Text('Viajar por bote'),
+              value: Transportation.boat,
+              groupValue: selectedTranportation,
+              onChanged: (value) => setState(() {
+                selectedTranportation = Transportation.boat;
+              }),
+            ),
+            RadioListTile(
+              title: const Text('By plane'),
+              subtitle: const Text('Viajar por avión'),
+              value: Transportation.plane,
+              groupValue: selectedTranportation,
+              onChanged: (value) => setState(() {
+                selectedTranportation = Transportation.plane;
+              }),
+            ),
+            RadioListTile(
+              title: const Text('By submarino'),
+              subtitle: const Text('Viajar por submarino'),
+              value: Transportation.submarine,
+              groupValue: selectedTranportation,
+              onChanged: (value) => setState(() {
+                selectedTranportation = Transportation.submarine;
+              }),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
