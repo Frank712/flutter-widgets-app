7625b0186c8945e9ac89cac6df006476075b042a
Francisco Lino <franksuriel_7@hotmail.com>
2023-04-11 02:12:39 -0600

    Finish infinite scroll

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -57,4 +57,10 @@ const appMenuItems = <MenuItem>[
     link: '/tutorial',
     icon: Icons.accessible_sharp,
   ),
+  MenuItem(
+    title: 'Infinite Scroll and Pull',
+    subtitle: 'Inifinite list and pull to refresh',
+    link: '/infinite',
+    icon: Icons.list_alt_rounded,
+  ),
 ];
